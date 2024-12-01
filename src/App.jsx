4971a93b3f540bc538cade6f3f93f6f4adb3493a
Kushang Tanawala <kushangtanawala24@gmail.com>
2024-12-01 13:31:23 +0530

    Check The Faced Smile And Give Message About The Face Smile

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import SmileCapture from './components/SmileCapture';
 import SmilePosts from './components/SmilePosts';
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -12,14 +13,11 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (images.length > 0) {
-      localStorage.setItem('smiles', JSON.stringify(images));
-    }
+    localStorage.setItem('smiles', JSON.stringify(images));
   }, [images]);
 
   const addImage = (image, isPerfectSmile, isAngry) => {
     if (isAngry) {
-      toast('Don\'t be angry, keep smiling!');
       return;
     }
 
@@ -27,18 +25,22 @@ function App() {
       const updatedImages = [...prevImages, { image, isPerfectSmile, isAngry }];
       return updatedImages;
     });
-    toast(isPerfectSmile ? 'Great smile! Tokens awarded!' : 'Smile detected, but not perfect. Retake the photo!');
   };
 
   const deleteImage = (index) => {
     const updatedImages = images.filter((_, i) => i !== index);
     setImages(updatedImages);
-    localStorage.setItem('smiles', JSON.stringify(updatedImages));
-    toast('Image deleted!');
+    toast.info('Image deleted!');
   };
 
   return (
     <div className="container text-center mt-4">
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        style={{ width: "auto" }}
+      />
       <h1 className="my-3">Capture a Good Smile! 🤗</h1>
       <SmileCapture addImage={addImage} />
       <SmilePosts images={images} deleteImage={deleteImage} />
